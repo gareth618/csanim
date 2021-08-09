88e656b20bcd3ed11b2c7ce390b22bf2238264fa
Gareth618 <bloggareth@gmail.com>
2021-08-09 17:14:54 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import p5 from 'p5';
+import sketch from './sketch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default function App() {
+  const ref = React.useRef(null);
+  React.useEffect(() => {
+    const myP5 = new p5(sketch, ref.current);
+    ref.current.children[0].style.borderRadius = '.5rem';
+    if (myP5.mousePressed != null) {
+      const mousePressed = myP5.mousePressed;
+      myP5.mousePressed = null;
+      ref.current.addEventListener('click', () => {
+        mousePressed();
+      });
+    }
+    if (myP5.keyTyped != null) {
+      const keyTyped = myP5.keyTyped;
+      myP5.keyTyped = null;
+      document.addEventListener('keydown', () => {
+        if (!(0 <= myP5.mouseX && myP5.mouseX <= myP5.width)) return;
+        if (!(0 <= myP5.mouseY && myP5.mouseY <= myP5.height)) return;
+        keyTyped();
+      });
+    }
+  }, [ref]);
+  return <div style={{ width: 'fit-content', margin: '1rem auto' }} ref={ref} />;
+};
